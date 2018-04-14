3dd3a7457c92bdc257eb7deb47916fd37a957720
Gerson <gersonm17@gmail.com>
2018-04-13 19:21:27 -0500

    Modificaciones mayores

--- src/ApiResponse.php
+++ src/ApiResponse.php
@@ -15,6 +15,7 @@
 		public $errors = [];
 		public $httpStatus;
 		public $headers = [];
+		public $data;
 		
 		public static $SUCCESS = 'success';
 		public static $ERROR = 'error';
@@ -23,63 +24,69 @@
 		public static $EDITED = 'edited';
 		public static $CREATED = 'created';
 		public static $DELETED = 'deleted';
+		public static $BAD_REQUEST = 'bad_request';
 		
 		private $tpls = [
-			'success'   => [ 'code' => 'success', 'msg' => 'Petición correcta', 'httpStatus' => 200 ],
-			'error'   => [ 'code' => 'error', 'msg' => 'Ocurrió un problema al realizar tu petición', 'httpStatus' => 500 ],
+			'success'     => [ 'code' => 'success', 'msg' => 'Petición correcta', 'httpStatus' => 200 ],
+			'error'       => [ 'code' => 'error', 'msg' => 'Ocurrió un problema al realizar tu petición', 'httpStatus' => 500 ],
 			'not_found'   => [ 'code' => 'error', 'msg' => 'No pudimos encontrar este item, por favor prueba nuevamente', 'httpStatus' => 404 ],
 			'need_params' => [ 'code' => 'error', 'msg' => 'Algúnos parametros necesario no fueron recibidos', 'httpStatus' => 400 ],
-			'edited' => [ 'code' => 'success', 'msg' => 'Los cambios fueron guardados correctamente', 'httpStatus' => 200 ],
-			'created' => [ 'code' => 'success', 'msg' => 'El registro fué creado', 'httpStatus' => 201 ],
-			'deleted' => [ 'code' => 'success', 'msg' => 'El registro fué eliminado', 'httpStatus' => 200]
+			'edited'      => [ 'code' => 'success', 'msg' => 'Los cambios fueron guardados correctamente', 'httpStatus' => 200 ],
+			'created'     => [ 'code' => 'success', 'msg' => 'El registro fué creado', 'httpStatus' => 201 ],
+			'deleted'     => [ 'code' => 'success', 'msg' => 'El registro fué eliminado', 'httpStatus' => 200 ],
+			'bad_request' => [ 'code' => 'bad_request', 'msg' => 'Uno o más datos enviados no cumplen los requisitos', 'httpStatus' => 400 ]
 		];
 		
-		public function __construct( $msg = null, $code = null, $httpStatus = null, $details = null ) {
+		public function __construct( $msg = null, $code = null, $httpStatus = null, $details = null, $data = null ) {
 			
 			$this->details    = $details;
-			$this->msg        = $msg?$msg:$this->tpls['success']['msg'];
-			$this->code       = $code?$code:$this->tpls['success']['code'];
-			$this->httpStatus = $httpStatus?$httpStatus:$this->tpls['success']['httpStatus'];
+			$this->msg        = $msg ? $msg : $this->tpls['success']['msg'];
+			$this->code       = $code ? $code : $this->tpls['success']['code'];
+			$this->httpStatus = $httpStatus ? $httpStatus : $this->tpls['success']['httpStatus'];
+			$this->data       = $data;
 		}
 		
-		public function toResource($data = null) {
+		public function toAdditional( $data = null ) {
 			$response = [
 				'status' => [
 					'code'    => $this->code,
 					'msg'     => $this->msg,
 					'details' => $this->details,
-					'errors' => $this->errors
+					'errors'  => $this->errors
 				]
 			];
-			if($data)
+			if ( $data ) {
 				$response['data'] = $data;
+			}
 			
 			return $response;
 		}
 		
-		public function setType( string $type, $msg = null, $details = null ) {
+		public function loadTemplate( string $template, $msg = null, $details = null ) {
 			
 			$this->details = $details;
-			if ( isset( $this->tpls[ $type ] ) ) {
-				$this->msg        = $msg?$msg:$this->tpls[$type]['msg'];
-				$this->code       = $this->tpls[$type]['code'];
-				$this->httpStatus = $this->tpls[$type]['httpStatus'];
+			if ( isset( $this->tpls[ $template ] ) ) {
+				$this->msg        = $msg ? $msg : $this->tpls[ $template ]['msg'];
+				$this->code       = $this->tpls[ $template ]['code'];
+				$this->httpStatus = $this->tpls[ $template ]['httpStatus'];
 			}
 		}
 		
-		public function httpResponse( $template = null, $msg = null, $details = null, $data = null) {
-			if($template)
-				$this->setType($template, $msg, $details);
-			return response( $this->toResource($data), $this->httpStatus, $this->headers );
+		public function httpResponse( $template = null, $msg = null, $details = null, $data = null ) {
+			if ( $template ) {
+				$this->loadTemplate( $template, $msg, $details );
+			}
+			
+			return response( $this->toAdditional( $data ), $this->httpStatus, $this->headers );
 		}
 		
 		static function make( $msg = null, $code = null, $httpStatus = null, $details = null, $data = null ) {
-			$status = new ApiStatus( $msg, $code, $httpStatus, $details );
-			return $status->toResource($data);
+			$status = new ApiResponse( $msg, $code, $httpStatus, $details );
+			
+			return $status->toAdditional( $data );
 		}
 		
-		public function registerError($errorMsg)
-		{
-			array_push($this->errors, $errorMsg);
+		public function registerError( $errorMsg ) {
+			array_push( $this->errors, $errorMsg );
 		}
 	}
